0e5e3298ea222f2e862fa0b6c8fa7b2cd2f19388
anonymous-sys19 <70624994+anonymous-sys19@users.noreply.github.com>
2020-10-18 18:44:57 +0000

    Add files via upload

---
+++ AGENDA/agenda.py
@@ -0,0 +1,41 @@
+print'@a.n.o.n.m.o@@@@@@@@'
+print'@by:anonimo:HACKERPC@'
+
+
+
+#python_version 2.7
+
+
+print("Bienvenido a Agenda Telefonica")
+print("Que operacion decea realizar?")
+print(" 1.- Crear contacto nuevo")
+print (" 2.- Listar contactos")
+opcion = (input("> "))
+
+if opcion == 1 :
+	print("Crear contacto nuevo")
+	agenda = open ("files/base.csv", 'a')
+
+	nombre = raw_input( "ingrese nombre del contacto: ")
+	numero = raw_input ("Ingrese numero de contacto: ")
+
+	agenda.write(nombre)
+	agenda.write(" : ")
+	agenda.write(numero)
+	agenda.write(" : ")
+	agenda.write("\n")
+	
+	print (" GRACIAS ADIOS .. ")
+	agenda.close()
+
+elif opcion == 2:
+	print("Lista de contacto: ")
+	agenda = open ("files/base.csv")
+	cantidad = int(raw_input ("Cantidad de contactos a listar: " ))
+	for i in range(0, cantidad):
+		print(agenda.readline())
+		
+	print("RESULTADO " + "ADIOS")
+	agenda.close()
+else:
+	print("Opcion no valida")
